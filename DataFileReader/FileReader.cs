659f59c5b7048c067e23223d5c2d7f12a8cea7c5
ogiladi <10680708+ogiladi@users.noreply.github.com>
2019-03-10 15:55:09 -0600

    initial commit

---
+++ DataFileReader/FileReader.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DataFileReader
+{
+    /// <summary>
+    /// A file reader class. Reads a data file that contains one number
+    /// on each row. That data is then stored in a list. The class also includes
+    /// methods to compute the sum, average, and standard deviation of the numbers 
+    /// in the file. Finally, the class contains a method to present the file content
+    /// on the screen.
+    /// </summary>
+    public class FileReader
+    {
+        private readonly string _filePath;
+        private List<double> _inputNums;
+
+        public FileReader(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        /// <summary>
+        /// Reads a file from a file path and returns a list with the lines
+        /// of the file
+        /// </summary>
+        /// <returns>The list of lines as strings, null if an error occured.</returns>
+        /// <param name="filePath">File path.</param>
+        private string[] GetListNums(string filePath)
+        {
+            try
+            {
+                return File.ReadAllLines(@filePath, Encoding.UTF8);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Builds a list of numbers from an array of strings. If one or more of the entries
+        /// is invalid, the function returns null.
+        /// </summary>
+        /// <returns>The list of numbers, null if an error occured.</returns>
+        /// <param name="lines">Lines.</param>
+        private List<double> BuildListNumbers(string[] lines)
+        {
+            try
+            {
+                if (lines == null || lines.Length == 0)
+                    return null;
+
+                List<double> listNums = new List<double>();
+
+                foreach (var line in lines)
+                {
+                    var num = double.Parse(line);
+                    listNums.Add(num);
+                }
+                return listNums;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Sets the list of numbers.  
+        /// </summary>
+        /// <returns><c>true</c>, if list numbers was set, <c>false</c> otherwise.</returns>
+        public bool SetListNumbers()
+        {
+            try
+            {
+                var lines = GetListNums(_filePath);
+                _inputNums = BuildListNumbers(lines);
+
+                return (_inputNums != null);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Computes the sum of a list of numbers.
+        /// </summary>
+        /// <returns>The sum.</returns>
+        public double Sum()
+        {
+            return _inputNums.Sum();
+        }
+
+        /// <summary>
+        /// Computes the average of a list of numbers.
+        /// </summary>
+        /// <returns>The average.</returns>
+        public double Average()
+        {
+            return _inputNums.Average();
+        }
+
+        /// <summary>
+        /// Compute the standard deviation of a list of numbers.
+        /// </summary>
+        /// <returns>The std. dev.</returns>
+        public double StdDev()
+        {
+            double result = 0;
+            int count = _inputNums.Count;
+            if (count > 1)
+            {
+                double avg = _inputNums.Average();
+                double sum = _inputNums.Sum(d => Math.Pow(d - avg, 2));
+
+                result = Math.Sqrt(sum / (count - 1));
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Shows the file as a string.
+        /// </summary>
+        /// <returns>The file as a string.</returns>
+        public string ShowFile()
+        {
+            StringBuilder sb = new StringBuilder("Row\tValue\n");
+
+            for (int i = 0; i < _inputNums.Count - 1; i++)
+            {
+                sb.Append(string.Format("{0}\t{1}\n", i + 1, _inputNums[i]));
+            }
+
+            if (_inputNums.Count > 0)
+                sb.Append(string.Format("{0}\t{1}", _inputNums.Count, _inputNums[_inputNums.Count - 1]));
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ DataFileReader/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace DataFileReader
+{
+    class Program
+    {
+        private static FileReader _reader;
+
+        private static void Main(string[] args)
+        {
+            Console.Write("Welcome user! This little program is meant to help you read " +
+                "a data file and make some simple calculations.\n");
+                
+            bool outerLoop = true;
+
+            while (outerLoop)
+            {
+                Console.Write("\nPlease enter the path of the file that you would like to read. " +
+                "Please make sure that:\n" +
+                "1. You enter the full file path.\n" +
+                "2. The file is a txt file which contains a single number on each row.\n" +
+                "3. The file contains at least one line of data.\n" +
+                "Now please go ahead and enter the path (enter x to exit the program): ");
+
+                var input = Console.ReadLine();
+                if (input.Equals("x"))
+                {
+                    outerLoop = false;
+                    break;
+                }
+
+                _reader = new FileReader(input);
+                var success = _reader.SetListNumbers();
+
+                if (!success)
+                {
+                    Console.Write("\n\nThere has been an error reading the file. Make sure that the " +
+                        "path is valid and that the file contains valid data.\n\n");
+                }
+                else
+                {
+                    Console.Write("\n\nExcellent news! We have been able to read your file.\n\n");
+                    bool innerLoop = true;
+                    while (innerLoop)
+                    {
+                        Console.Write("Please choose one of the following options:\n" +
+                            "-Enter 1 to compute the sum.\n" +
+                            "-Enter 2 to compute the average.\n" +
+                            "-Enter 3 to compute the standard deviation.\n" +
+                            "-Enter 4 to view the numbers in the file.\n" +
+                            "-Enter 5 to enter another file path.\n" +
+                            "-Enter x to exit the program.\n");                    
+                        Console.Write("Enter your choice here: ");
+                        var selection = Console.ReadLine();
+
+                        switch (selection)
+                        {
+                            case "1":
+                                Console.WriteLine(string.Format("\nThe sum is {0}\n", _reader.Sum()));
+                                break;
+                            case "2":
+                                Console.WriteLine(string.Format("\nThe average is {0}\n", _reader.Average()));
+                                break;
+                            case "3":
+                                Console.WriteLine(string.Format("\nThe standard deviation is {0}\n", _reader.StdDev()));
+                                break;
+                            case "4":
+                                Console.WriteLine(string.Format("\nHere is the file:\n{0}\n", _reader.ShowFile()));
+                                break;
+                            case "5":
+                                innerLoop = false;
+                                break;
+                            case "x":
+                                innerLoop = false;
+                                outerLoop = false;
+                                break;
+                            default:
+                                Console.Write("\nSorry, invalid choice. Please try again.\n");
+                                break;
+                            
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("\nG'bye! Hoping to see you again soon.\n");
+        }
+    }
+}
